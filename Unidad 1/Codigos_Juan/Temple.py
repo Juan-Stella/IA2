0b88fae248afeb96532cf1a26fd27bebaf3b2bce
Juan-Stella <juanstella96@gmail.com>
2026-03-11 17:11:28 -0300

    Optimizar algoritmo de Temple Simulado (agregar cache de distancias y variables globales)

--- Unidad 1/Codigos_Juan/Temple.py
+++ Unidad 1/Codigos_Juan/Temple.py
@@ -8,10 +8,47 @@ ordenes = []
 
 with open(r"D:\01. Facultad\ia2\IA2\IA2\Unidad 1\Codigos_Juan\ordenes.csv", "r") as archivo:
     lector = csv.reader(archivo)
-
     for fila in lector:
         ordenes.append([int(x) for x in fila])
 
+MAPA_ESTANTERIAS = {
+    1: (1, 2),  2: (1, 3),   3: (2, 2),  4: (2, 3),
+    5: (3, 2),  6: (3, 3),   7: (4, 2),  8: (4, 3),
+    9: (1, 6),  10: (1, 7),  11: (2, 6), 12: (2, 7),
+    13: (3, 6), 14: (3, 7),  15: (4, 6), 16: (4, 7),
+    17: (1, 10), 18: (1, 11), 19: (2, 10), 20: (2, 11),
+    21: (3, 10), 22: (3, 11), 23: (4, 10), 24: (4, 11),
+    25: (6, 2), 26: (6, 3),  27: (7, 2), 28: (7, 3),
+    29: (8, 2), 30: (8, 3),  31: (9, 2), 32: (9, 3),
+    33: (6, 6), 34: (6, 7),  35: (7, 6), 36: (7, 7),
+    37: (8, 6), 38: (8, 7),  39: (9, 6), 40: (9, 7),
+    41: (6, 10), 42: (6, 11), 43: (7, 10), 44: (7, 11),
+    45: (8, 10), 46: (8, 11), 47: (9, 10), 48: (9, 11),
+}
+
+def estanteria_a_coord(num):
+    return MAPA_ESTANTERIAS.get(num)
+
+cache_distancias = {}
+
+def obtener_distancia(est_origen, est_destino, astar):
+    if est_origen == est_destino:
+        return 0
+    par = tuple(sorted((est_origen, est_destino)))
+    
+    if par not in cache_distancias:
+        start = estanteria_a_coord(par[0])
+        finish = estanteria_a_coord(par[1])
+        camino = astar.busqueda(start, finish)
+        cache_distancias[par] = len(camino) - 1 if camino else float('inf')
+        
+    return cache_distancias[par]
+
+def calcular_costo_ruta(ruta, astar):
+    costo = 0
+    for j in range(len(ruta) - 1):
+        costo += obtener_distancia(ruta[j], ruta[j + 1], astar)
+    return costo
 
 def temple_simulado(solucion_inicial, iteraciones, T):
     grafo = np.zeros((11, 13), dtype=int)
@@ -23,21 +60,15 @@ def temple_simulado(solucion_inicial, iteraciones, T):
                     grafo[r, c] = 1
 
     astar = A.A_Star(grafo)
-
     solucion_actual = solucion_inicial.copy()
-
-    costo_actual = 0
-    for j in range(len(solucion_actual) - 1):
-        start = estanteria_a_coord(solucion_actual[j])
-        finish = estanteria_a_coord(solucion_actual[j + 1])
-        camino = astar.busqueda(start, finish)
-        costo_actual += len(camino) - 1
+    
+    costo_actual = calcular_costo_ruta(solucion_actual, astar)
 
     historial_costos = []
     historial_temperatura = []
 
     for i in range(iteraciones):
-        T = T *0.97
+        T = T * 0.96
         if T < 0.0001:
             break
 
@@ -47,18 +78,17 @@ def temple_simulado(solucion_inicial, iteraciones, T):
         a, b = random.sample(range(len(vecino)), 2)
         vecino[a], vecino[b] = vecino[b], vecino[a]
 
-        costo_vecino = 0
-        for j in range(len(vecino) - 1):
-            start = estanteria_a_coord(vecino[j])
-            finish = estanteria_a_coord(vecino[j + 1])
-            camino = astar.busqueda(start, finish)
-            costo_vecino += len(camino) - 1
+        costo_vecino = calcular_costo_ruta(vecino, astar)
 
         if costo_vecino < costo_actual:
             solucion_actual = vecino
             costo_actual = costo_vecino
         else:
-            p = np.exp((costo_actual - costo_vecino) / T)
+            try:
+                p = np.exp((costo_actual - costo_vecino) / T)
+            except OverflowError:
+                p = 0
+                
             if np.random.rand() < p:
                 solucion_actual = vecino
                 costo_actual = costo_vecino
@@ -69,43 +99,6 @@ def temple_simulado(solucion_inicial, iteraciones, T):
         print("Iteración:", i, "Costo:", costo_actual, "T:", T)
 
     return solucion_actual, costo_actual, historial_costos, historial_temperatura
-
-
-def estanteria_a_coord(num):
-    mapa = {
-        1: (1, 2),  2: (1, 3),
-        3: (2, 2),  4: (2, 3),
-        5: (3, 2),  6: (3, 3),
-        7: (4, 2),  8: (4, 3),
-
-        9: (1, 6),  10: (1, 7),
-        11: (2, 6), 12: (2, 7),
-        13: (3, 6), 14: (3, 7),
-        15: (4, 6), 16: (4, 7),
-
-        17: (1, 10), 18: (1, 11),
-        19: (2, 10), 20: (2, 11),
-        21: (3, 10), 22: (3, 11),
-        23: (4, 10), 24: (4, 11),
-
-        25: (6, 2), 26: (6, 3),
-        27: (7, 2), 28: (7, 3),
-        29: (8, 2), 30: (8, 3),
-        31: (9, 2), 32: (9, 3),
-
-        33: (6, 6), 34: (6, 7),
-        35: (7, 6), 36: (7, 7),
-        37: (8, 6), 38: (8, 7),
-        39: (9, 6), 40: (9, 7),
-
-        41: (6, 10), 42: (6, 11),
-        43: (7, 10), 44: (7, 11),
-        45: (8, 10), 46: (8, 11),
-        47: (9, 10), 48: (9, 11),
-    }
-
-    return mapa.get(num)
-
 
 def graficar_curvas(historial_costos, historial_temperatura):
     plt.figure(figsize=(10, 5))
@@ -118,7 +111,6 @@ def graficar_curvas(historial_costos, historial_temperatura):
     plt.grid(True)
     plt.show()
 
-
 def main():
     solucion, costo, historial_costos, historial_temperatura = temple_simulado(ordenes[1], 1000, 100)
 
@@ -126,7 +118,6 @@ def main():
     print("Costo total:", costo)
 
     graficar_curvas(historial_costos, historial_temperatura)
-
 
 if __name__ == "__main__":
     main()
